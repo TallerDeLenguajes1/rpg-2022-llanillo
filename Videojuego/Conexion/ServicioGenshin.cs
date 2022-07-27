30d257864011cf608be4272d0e18a6f8a0838a9e
Octavio Llanillo <llanillo4@hotmail.com>
2022-07-27 18:08:12 -0300

    Cambios menores

--- Videojuego/Conexion/ServicioGenshin.cs
+++ Videojuego/Conexion/ServicioGenshin.cs
@@ -5,25 +5,28 @@ namespace Videojuego.Conexion;
 
 public static class ServicioGenshin
 {
-    private const string JsonType = "application/json";
-    private const string Url = "https://api.genshin.dev/characters";
+    private const string TipoContenido = "application/json";
+    private const string ApiUrl = "https://api.genshin.dev/characters";
+    private const string Metodo = "GET";
         
-    public static List<string>? VerGenshinNombres()
+    public static List<string>? VerResultadosApi()
     {
         List<string>? nombres = null;
         
-        var request = (HttpWebRequest) WebRequest.Create(Url);
-        request.Method = "GET";
-        request.ContentType = JsonType;
-        request.Accept = JsonType;
+        var request = (HttpWebRequest) WebRequest.Create(ApiUrl);
+        request.Method = Metodo;
+        request.ContentType = TipoContenido;
+        request.Accept = TipoContenido;
 
         try
         {
-            using WebResponse respuesta = request.GetResponse();
-            using Stream streamReader = respuesta.GetResponseStream(); 
-            using StreamReader objReader = new StreamReader(streamReader);
-            string responseBody = objReader.ReadToEnd();
+            using WebResponse respuestaWeb = request.GetResponse();
+            using Stream stream = respuestaWeb.GetResponseStream(); 
+            using var streamReader = new StreamReader(stream);
+            
+            string responseBody = streamReader.ReadToEnd();
             nombres = JsonSerializer.Deserialize<List<string>>(responseBody);
+            
         }catch(WebException e){
             Console.WriteLine(e.ToString());
         }
